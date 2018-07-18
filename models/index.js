85cd91421fbee66a30acab1c82a31141777d56cf
TonyB <tbtonyibrown@outlook.com>
2018-07-17 20:52:36 -0400

    latest

--- models/index.js
+++ models/index.js
@@ -9,7 +9,9 @@ var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+    console.log('process.env debug', process.env[config.use_env_variable]);
+    console.log('config debug', config);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], root, '');
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
